1981bb440055c5b73debba0d2be74ee0351b1cbc
rexoriousyun <chgchg123@naver.com>
2017-12-11 22:12:36 -0500

    downloaded local json file

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import CurrentTime from './CurrentTime.js';
 import OperationTime from './OperationTime.js';
+import statusJson from '../system_status.json';
 import Logo from '../img/akira_logo.jpg';
 import './App.css';
-
 
 export default class App extends Component {
   constructor(props) {
@@ -51,34 +51,58 @@ export default class App extends Component {
       }
       return [hour, minute, day]
     }
-
-    fetch('https://app.akira.md/api/system_status.json')
-    .then(results => {
-      return results.json();
-    }).then(json => {
-      this.setState({
-        data: json,
-        directSignupAllowed: json['direct_signup_allowed'],
-        isOpenForBusiness: json['is_open_for_business'],
-        online: json['online'],
-        openHoursToday: {
-          closeAt: {
-            hour: timeClass(json['open_hours_today']['close_at'])[0],
-            minute: timeClass(json['open_hours_today']['close_at'])[1],
-            day: timeClass(json['open_hours_today']['close_at'])[2]
-          },
-          openAt: {
-            hour: timeClass(json['open_hours_today']['open_at'])[0],
-            minute: timeClass(json['open_hours_today']['open_at'])[1],
-            day: timeClass(json['open_hours_today']['open_at'])[2]
-          }
+    // Commented out due to Access-Control-Allow-Origin header problem.
+    //
+    // fetch('https://app.akira.md/api/system_status.json')
+    // .then(results => {
+    //   return results.json();
+    // }).then(json => {
+    //   this.setState({
+    //     data: json,
+    //     directSignupAllowed: json['direct_signup_allowed'],
+    //     isOpenForBusiness: json['is_open_for_business'],
+    //     online: json['online'],
+    //     openHoursToday: {
+    //       closeAt: {
+    //         hour: timeClass(json['open_hours_today']['close_at'])[0],
+    //         minute: timeClass(json['open_hours_today']['close_at'])[1],
+    //         day: timeClass(json['open_hours_today']['close_at'])[2]
+    //       },
+    //       openAt: {
+    //         hour: timeClass(json['open_hours_today']['open_at'])[0],
+    //         minute: timeClass(json['open_hours_today']['open_at'])[1],
+    //         day: timeClass(json['open_hours_today']['open_at'])[2]
+    //       }
+    //     },
+    //     systemTime: {
+    //       hour: timeClass(json['system_time'])[0],
+    //       minute: timeClass(json['system_time'])[1],
+    //       day: timeClass(json['system_time'])[2]
+    //     }
+    //   })
+    // })
+    this.setState({
+      data: statusJson,
+      directSignupAllowed: statusJson['direct_signup_allowed'],
+      isOpenForBusiness: statusJson['is_open_for_business'],
+      online: statusJson['online'],
+      openHoursToday: {
+        closeAt: {
+          hour: timeClass(statusJson['open_hours_today']['close_at'])[0],
+          minute: timeClass(statusJson['open_hours_today']['close_at'])[1],
+          day: timeClass(statusJson['open_hours_today']['close_at'])[2]
         },
-        systemTime: {
-          hour: timeClass(json['system_time'])[0],
-          minute: timeClass(json['system_time'])[1],
-          day: timeClass(json['system_time'])[2]
+        openAt: {
+          hour: timeClass(statusJson['open_hours_today']['open_at'])[0],
+          minute: timeClass(statusJson['open_hours_today']['open_at'])[1],
+          day: timeClass(statusJson['open_hours_today']['open_at'])[2]
         }
-      })
+      },
+      systemTime: {
+        hour: timeClass(statusJson['system_time'])[0],
+        minute: timeClass(statusJson['system_time'])[1],
+        day: timeClass(statusJson['system_time'])[2]
+      }
     })
   };
 
